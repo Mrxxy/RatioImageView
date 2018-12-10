89611cd15106cc99a921ba2d3ed8568d5d8b703a
xiaoyichao <xiaoyichao@goldmantis.com>
2018-12-10 14:03:10 +0800

    init project

---
+++ app/src/main/java/mrxxy/github/com/ratioimageview/MainActivity.java
@@ -0,0 +1,20 @@
+package mrxxy.github.com.ratioimageview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import mrxxy.github.ratioimageview.RatioImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RatioImageView ratioImageView = findViewById(R.id.ratio_image);
+        ratioImageView.setRatio(16 / 9.0f);
+    }
+
+}
---
+++ library/src/main/java/mrxxy/github/ratioimageview/RatioImageView.java
@@ -0,0 +1,67 @@
+package mrxxy.github.ratioimageview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+public class RatioImageView extends android.support.v7.widget.AppCompatImageView {
+
+    private static final String TAG = RatioImageView.class.getSimpleName();
+
+    private float ratio;
+
+    public RatioImageView(Context context) {
+        this(context, null);
+    }
+
+    public RatioImageView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RatioImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.RatioImageView, defStyleAttr, 0);
+
+        ratio = typedArray.getFloat(R.styleable.RatioImageView_ratio, 0);
+        if (ratio == 0) {
+            float ratioW = typedArray.getFloat(R.styleable.RatioImageView_ratio_w, 0);
+            float ratioH = typedArray.getFloat(R.styleable.RatioImageView_ratio_h, 1);
+            ratio = ratioW / ratioH;
+        }
+
+        typedArray.recycle();
+    }
+
+    /**
+     * 设置比例
+     *
+     * @param ratio 宽高比
+     */
+    public void setRatio(float ratio) {
+        this.ratio = ratio;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int widthPadding = getPaddingStart() + getPaddingEnd();
+        int heightPadding = getPaddingTop() + getPaddingBottom();
+        int width = MeasureSpec.getSize(widthMeasureSpec) - widthPadding;
+        int height = MeasureSpec.getSize(heightMeasureSpec) - heightPadding;
+        if (ratio != 0) {
+            if (widthMode == MeasureSpec.EXACTLY) {
+                // 宽度确定时计算新高度
+                height = (int) (width / ratio + 0.5f) + heightPadding;
+                heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+            } else if (heightMode == MeasureSpec.EXACTLY) {
+                // 高度确定时计算新宽度
+                width = (int) (height * ratio + 0.5f) + widthPadding;
+                widthMeasureSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY);
+            }
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+}
